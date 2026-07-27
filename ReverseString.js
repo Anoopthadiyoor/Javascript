2b22c3aaba7f8c3541aefef963620db44f9db2e0
Anoop krishnan <anoopthadiyoor123@gmail.com>
2026-07-27 20:49:45 +0530

    Create ReverseString.js

---
+++ ReverseString.js
@@ -0,0 +1,6 @@
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(reverseString("hello")); // Output: "olleh"
+console.log(reverseString("world")); // Output: "dlrow"
