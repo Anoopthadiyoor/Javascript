b578aebad3c8692dee9803c736698b4f33ae5457
Anoop krishnan <anoopthadiyoor123@gmail.com>
2026-08-03 19:22:37 +0530

    Add string concatenation examples in String_concatenation.js

---
+++ String_concatenation.js
@@ -0,0 +1,12 @@
+const num1=70;
+console.log(typeof(num1))
+
+const numToStr=num1.toString()
+console.log(typeof(numToStr))
+
+console.log(numToStr+10)
+
+
+const str3="10"
+const num3=10
+console.log(num3+Number(str3))
