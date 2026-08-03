8db4dcb84b0a80b1634daa5c32ade96845240cbf
Anoop krishnan <anoopthadiyoor123@gmail.com>
2026-08-03 19:25:33 +0530

    Add type conversion examples in type-conversion.js

---
+++ type-conversion.js
@@ -0,0 +1,9 @@
+let value = "123.45";
+
+
+console.log( Number(value));          
+console.log( parseInt(value));    
+console.log( parseFloat(value));  
+console.log( String(value));          
+console.log( Boolean(value));       
+console.log( isNaN(Number(value))); 
